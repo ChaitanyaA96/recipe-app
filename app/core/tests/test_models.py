905048e2f9b64271a802101a3759bfc59933cb4c
ChaitanyaA96 <chaitanya.attarde46@gmail.com>
2024-09-02 09:58:06 -0400

    Image API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from decimal import Decimal
 from core import models
+from unittest.mock import patch
 
 
 def create_user(email='user@example.com', password='testuser123'):
@@ -82,3 +83,12 @@ class ModelTests(TestCase):
             name='Ingredient 1'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test creating a recipe image file"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -91,4 +91,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """"Serializer for Recipe details"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for Recipe images"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
+        extra_kwargs = {'image': {'required': True}}
